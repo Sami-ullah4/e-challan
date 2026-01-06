e47b51654429f233948e93c150c933a8bdfd5707
Sami-ullah4 <samiullah0313066@gmail.com>
2026-01-07 02:00:50 +0500

    sdfsdSdqasd

--- app/component/header/page.js
+++ app/component/header/page.js
@@ -1,19 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
+
 export default function Header() {
   return (
-    <header className=" w-full  shadow bg-white">
-      <nav className="px-3 lg:px-30 ">
+    <header className="w-full shadow bg-white">
+      <nav className="px-3 lg:px-30">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <Link href="/" className="flex items-center gap-3 text-black">
-            {/* <Image
-              src={"./logo.webp"}
-              alt="Hero Background"
-              width={1200} // desired width
-              height={600} // desired height
-              className="rounded-xl shadow-lg"
-            />{" "} */}
+          <Link href="/" className="flex items-center gap-3 text-black ">
+            <figure className="relative w-[42px] h-[42px] bg-pink-800">
+              <Image
+                src="/logo.webp"
+                alt="e-Challan Logo"
+                width={42}
+                height={42}
+                className="object-contain"
+                priority
+              />
+            </figure>
+
             <div className="leading-tight">
               <div className="font-semibold text-lg">e-Challan</div>
               <div className="text-xs text-black/70">Search & Pay</div>
@@ -21,36 +26,48 @@ export default function Header() {
           </Link>
 
           {/* Desktop Navigation */}
-          <nav className="hidden sm:flex items-center gap-8 text-sm font-medium">
+          <nav className="hidden sm:flex items-center gap-8 text-lg font-semibold">
             <Link
               href="/"
               className="text-black/90 hover:text-black transition"
             >
-              Home
+              PSCE E-Challan{" "}
             </Link>
-            <a
+            <Link
+              href="/"
+              className="text-black/90 hover:text-black transition"
+            >
+              E-Challan Punjab{" "}
+            </Link>
+            <Link
+              href="/"
+              className="text-black/90 hover:text-black transition"
+            >
+              E-Challan Sindh{" "}
+            </Link>
+            <Link
               href="/e-challan-forn"
               className="text-black/90 hover:text-black transition"
             >
               Search Challan
-            </a>
-            <a
+            </Link>
+            <Link
               href="#contact"
               className="text-black/90 hover:text-black transition"
             >
               Contact
-            </a>
+            </Link>
           </nav>
 
           {/* Desktop Button */}
           <div className="hidden sm:block">
-            <a
+            <Link
               href="/e-challan-forn"
-              className="bg-blue-600 hover:bg-blue-700 transition
-                         text-black px-5 py-2 rounded-md text-sm font-medium"
+              className="bg-blue-500
+                         text-white px-5 py-2 rounded-md text-base font-semibold"
             >
               Search
-            </a>
+            </Link>
           </div>
 
           {/* Mobile Menu Button */}
